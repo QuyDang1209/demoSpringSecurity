03c3f5e736027d4f818b5a6f44e5d782b7a9f49e
QuyDang1209 <dangphuocquy1996@gmail.com>
2024-05-31 11:20:14 +0700

    demoSpringSecurity

---
+++ src/main/java/org/example/demospringsecurityjwt/jwt/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package org.example.demospringsecurityjwt.jwt.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
